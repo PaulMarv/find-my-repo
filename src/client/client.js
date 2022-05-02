22d3b8b9d443401488850f433cc50f390361f5ee
PaulMarv <mpsalako@gmail.com>
2022-05-02 13:37:27 +0100

    new commit

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient('process.env.REACT_APP_URL', 'process.env.REACT_APP_PUBLIC_KEY')
+const supabase = createClient('https://xqssbnkxqzhoaztiqgxo.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhxc3Nibmt4cXpob2F6dGlxZ3hvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTEzMjk3MjgsImV4cCI6MTk2NjkwNTcyOH0.phxCpEhsm7EVsIGlFGv72Uft3C0N1lZSreIDkK00sqY')
 export {
     supabase
 }
